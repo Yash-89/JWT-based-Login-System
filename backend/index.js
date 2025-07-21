b7c8c48e2e5b1bb3f6ce2a4f7cda73052ec97e38
LAB 204 <LAB204@SAKEC.LOCAL>
2025-07-21 16:08:54 +0530

    updated index.js

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,25 @@
 import express from 'express';
 import cors from 'cors';
+import dotenv from 'dotenv';
+import bodyParser from 'body-parser';
+
+dotenv.config();
 
 const app = express();
 
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+try {
+    const PORT = process.env.PORT || 5000;
+
+    app.listen(PORT, () => {
+        console.log(`Server is listening on ${PORT}`);
+    });
+} catch (error) {
+    console.error(error);
+}

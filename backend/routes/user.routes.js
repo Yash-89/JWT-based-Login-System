1c8f30c1a686ecd0fda3784d9cfa577a2e4819c3
Yash-89 <yashproraj@gmail.com>
2025-07-26 05:54:12 +0530

    added login route

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,8 +1,9 @@
-import { registerUser } from "../controllers/user.controllers.js";
+import { loginUser, registerUser } from "../controllers/user.controllers.js";
 import { Router } from "express";
 
 const router = Router();
 
 router.route('/register').post(registerUser);
+router.route('/login').post(loginUser);
 
 export default router;
